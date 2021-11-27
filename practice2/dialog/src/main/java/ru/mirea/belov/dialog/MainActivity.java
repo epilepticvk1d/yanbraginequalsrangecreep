619124e27b4ced67f0d4c59b3c0b188d2db3433b
epilepticvk1d <95118105+epilepticvk1d@users.noreply.github.com>
2021-11-27 09:57:04 +0300

    Add files via upload

---
+++ practice2/dialog/src/main/java/ru/mirea/belov/dialog/MainActivity.java
@@ -0,0 +1,119 @@
+package ru.mirea.belov.dialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.Notification;
+import android.app.ProgressDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import android.os.Handler;
+import android.os.Message;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView currentDateTime;
+    Calendar dateAndTime=Calendar.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        currentDateTime=(TextView)findViewById(R.id.currentDateTime);
+        setInitialDateTime();
+    }
+
+    public void onClickShowDialog(View view) {
+        MyDialogFragment dialogFragment = new MyDialogFragment();
+        dialogFragment.show(getSupportFragmentManager(), "mirea");
+    }
+
+    public void onOkClicked() {
+        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Иду дальше\"!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void onNeutralClicked() {
+        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"На паузе\"!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void onCancelClicked() {
+        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Нет\"!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    // установка начальных даты и времени
+    private void setInitialDateTime() {
+
+        currentDateTime.setText(DateUtils.formatDateTime(this,
+                dateAndTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+                        | DateUtils.FORMAT_SHOW_TIME));
+    }
+
+    // отображаем диалоговое окно для выбора времени
+    public void onClickSetTime(View view) {
+        new TimePickerDialog(MainActivity.this, t,
+                dateAndTime.get(Calendar.HOUR_OF_DAY),
+                dateAndTime.get(Calendar.MINUTE), true)
+                .show();
+
+        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Изменить время\"!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    // установка обработчика выбора времени
+    TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            dateAndTime.set(Calendar.MINUTE, minute);
+            setInitialDateTime();
+        }
+    };
+
+    // отображаем диалоговое окно для выбора даты
+    public void onClickSetDate(View view) {
+        new DatePickerDialog(MainActivity.this, d,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH))
+                .show();
+
+        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Изменить Дату\"!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    // установка обработчика выбора даты
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateAndTime.set(Calendar.YEAR, year);
+            dateAndTime.set(Calendar.MONTH, monthOfYear);
+            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDateTime();
+        }
+    };
+
+    // установка обработчика загрузки
+    public void onClickSetProgress(View view) {
+        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Загрузка...");
+        progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                progressDialog.dismiss();//закрываем dialog
+            }
+        });
+    }
+}
